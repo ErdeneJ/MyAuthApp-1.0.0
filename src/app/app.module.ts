afe10a228150002c72a3de330ade48a929ba1d9d
Erdene <35352749+ErdeneJ@users.noreply.github.com>
2018-02-18 22:13:47 -0700

    Adding the package
    Bringing in the Angular app files

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDMTIhjK4E2L759ocQznnKbiZLeWATnlk0',
+  authDomain: 'database-68d04.firebaseapp.com',
+  databaseURL: 'https://database-68d04.firebaseio.com',
+  storageBucket: 'database-68d04.appspot.com',
+  messagingSenderId: '838939271114'
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
